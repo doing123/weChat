729664aab42ae5134fd108496a74ef2f8e05e3b9
qinjunshan <13782533791@163.com>
2017-12-20 21:08:10 +0800

    weChat exercise002

--- ReaderMovie/pages/welcome/welcome.js
+++ ReaderMovie/pages/welcome/welcome.js
@@ -1,1 +1,15 @@
-Page({});
+Page({
+  onTap: function (event) {
+    wx.switchTab({
+      url: "../posts/post"
+    });
+    // wx.switchTab({
+    //   url: '/pages/posts/post',
+    // });
+
+    // wx.navigateTo({
+    //   url: '../posts/post'
+    // });
+
+  }
+});
